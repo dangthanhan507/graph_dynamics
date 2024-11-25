4cc29d760a009bb7bce4e5c02ea9eb19aa1fb89c
dangthanhan507 <dangthanhan507@gmail.com>
2024-11-24 23:37:32 -0500

    Added ability to visualize camera with opencv integrated with drake system.

--- src/hardware/teleop_utils.py
+++ src/hardware/teleop_utils.py
@@ -156,6 +156,55 @@ def teleop_diagram(meshcat, kuka_frame_name="iiwa_link_7"):
     
     return diagram, scene_graph
 
+LINE_LENGTH = 5
+CENTER_COLOR = (0, 255, 0)
+CORNER_COLOR = (255, 0, 255)
+
+def plotPoint(image, center, color):
+    center = (int(center[0]), int(center[1]))
+    image = cv2.line(image,
+                     (center[0] - LINE_LENGTH, center[1]),
+                     (center[0] + LINE_LENGTH, center[1]),
+                     color,
+                     3)
+    image = cv2.line(image,
+                     (center[0], center[1] - LINE_LENGTH),
+                     (center[0], center[1] + LINE_LENGTH),
+                     color,
+                     3)
+    return image
+
+def plotText(image, center, color, text):
+    center = (int(center[0]) + 4, int(center[1]) - 4)
+    return cv2.putText(image, str(text), center, cv2.FONT_HERSHEY_SIMPLEX,
+                       1, color, 3)
+
+#given 4x4 pose, visualize frame on camera (RGB)
+def plotPose(image, pose, length=0.1, thickness=10, K = np.eye(3), dist=np.zeros(5)):
+    #NOTE: apriltag x-axis, points out of the tag plane
+    rvec = cv2.Rodrigues(pose[:3,:3])[0]
+    tvec = pose[:3,3]
+    
+    axis = np.float32([[length,0,0], [0,length,0], [0,0,length]]).reshape(-1,3)
+    imgpts, _ = cv2.projectPoints(axis, rvec, tvec, K, dist)
+    imgpts = imgpts.astype(int)
+    center, _ = cv2.projectPoints(np.float32([[0,0,0]]), rvec, tvec, K, dist)
+    center = tuple(center[0].ravel())
+    center = (int(center[0]), int(center[1]))
+    
+    image = cv2.line(image, center, tuple(imgpts[0].ravel()), (0,0,255), thickness=thickness) #red
+    image = cv2.line(image, center, tuple(imgpts[1].ravel()), (0,255,0), thickness=thickness)
+    image = cv2.line(image, center, tuple(imgpts[2].ravel()), (255,0,0), thickness=thickness)
+    return image
+
+def visualize_detections(image, detections):
+    for detect in detections:
+        image = plotPoint(image, detect.center, CENTER_COLOR)
+        image = plotText(image, detect.center, CENTER_COLOR, detect.tag_id)
+        for corner in detect.corners:
+            image = plotPoint(image, corner, CORNER_COLOR)
+    return image
+
 ## --- camera drake API ---
 class CameraTagPublisher(LeafSystem):
     def __init__(self, cameras: Cameras, tag_width: float = 0.056):
@@ -166,17 +215,20 @@ class CameraTagPublisher(LeafSystem):
         self.detector = apriltag.Detector()
         self.tag2kukabase = RigidTransform()
         self.cameras_datapoints = defaultdict(list)
-        
+        self.obs = None
         # take as input Kuka
         self.DeclareAbstractInputPort("tag2kukabase", Value(RigidTransform()))
-        self.DeclarePeriodicPublishEvent(period_sec=0.1, offset_sec=0.0, publish=self.SaveKukaPose)
+        self.DeclarePeriodicPublishEvent(period_sec=1.0/self.cameras.capture_fps, offset_sec=0.0, publish=self.SaveObservation)
+        self.DeclarePeriodicPublishEvent(period_sec=1.0/self.cameras.capture_fps, offset_sec=0.0, publish=self.SaveKukaPose)
+        self.DeclarePeriodicPublishEvent(period_sec=1.0/self.cameras.capture_fps, offset_sec=0.1, publish=self.VisualizeCameras)
         self.DeclareForcedPublishEvent(self.DetectTagEvent)
-        
+    def SaveObservation(self, context):
+        self.obs = self.cameras.get_obs(get_color=True, get_depth=False)
     def SaveKukaPose(self, context):
         self.tag2kukabase: RigidTransform = self.get_input_port().Eval(context)
         
     def DetectTagEvent(self, context):
-        obs = self.cameras.get_obs(get_color=True, get_depth=False)
+        obs = self.obs
         for cam_idx in range(self.n_cam):
             color = obs[f'color_{cam_idx}'][-1]
             gray = cv2.cvtColor(color, cv2.COLOR_BGR2GRAY)
@@ -190,3 +242,29 @@ class CameraTagPublisher(LeafSystem):
                     
         print(self.cameras_datapoints)
         print()
+    def VisualizeCameras(self, context):
+        assert self.n_cam == 4, "Only works for 4 cameras, doing a 2x2 visual."
+        obs = self.obs
+        color0 = obs['color_0'][-1]
+        detections = self.detector.detect(cv2.cvtColor(color0, cv2.COLOR_BGR2GRAY))
+        color0 = visualize_detections(color0, detections)
+        
+        color1 = obs['color_1'][-1]
+        detections = self.detector.detect(cv2.cvtColor(color1, cv2.COLOR_BGR2GRAY))
+        color1 = visualize_detections(color1, detections)
+        
+        color2 = obs['color_2'][-1]
+        detections = self.detector.detect(cv2.cvtColor(color2, cv2.COLOR_BGR2GRAY))
+        color2 = visualize_detections(color2, detections)
+        
+        color3 = obs['color_3'][-1]
+        detections = self.detector.detect(cv2.cvtColor(color3, cv2.COLOR_BGR2GRAY))
+        color3 = visualize_detections(color3, detections)
+        
+        top = cv2.hconcat([color0, color1])
+        bottom = cv2.hconcat([color2, color3])
+        fullrgb = cv2.vconcat([top, bottom])
+        
+        cv2.imshow('rgb', fullrgb)
+        if cv2.waitKey(1) == ord('q'):
+            return
