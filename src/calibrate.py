804ea5f7c5e5f26c8488b5a8ce459195f618a3fb
dangthanhan507 <dangthanhan507@gmail.com>
2024-11-29 18:57:39 -0500

    Added robust calibration method.

---
+++ src/calibrate.py
@@ -0,0 +1,69 @@
+import numpy as np
+from hardware.kuka import goto_joints_mp, curr_pose_mp
+import argparse
+import apriltag
+from tqdm import tqdm
+from hardware.cameras import Cameras
+# given list of joints, follow each joint and take 10 seconds to go to each joint
+from collections import defaultdict
+import cv2
+
+if __name__ == '__main__':
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument("--file", type=str, default="../calib_data/joints/joint_pos.npy")
+    args = argparser.parse_args()
+    
+    joints = np.load(args.file) #(N, 7)
+    
+    cameras = Cameras(
+        WH=[640, 480],
+        capture_fps=15,
+        obs_fps=30,
+        n_obs_steps=2,
+        enable_color=True,
+        enable_depth=True,
+        process_depth=True
+    )
+    cameras.start(exposure_time=10)
+    detector = apriltag.Detector()
+    camera_datapoints = defaultdict(list)
+    
+    print(joints.shape)
+    for i in tqdm(range(joints.shape[0])):
+        goto_joints_mp(joints[i, :], endtime=30.0, joint_speed=2.0 * np.pi / 180.0)
+        pose = curr_pose_mp("../config/calib_med.yaml", "calibration_frame")
+        pt3d = pose.translation()
+        obs = cameras.get_obs()
+        
+        for i in range(cameras.n_fixed_cameras):
+            color = obs[f'color_{i}'][-1]
+            detections = detector.detect(cv2.cvtColor(color, cv2.COLOR_BGR2GRAY))
+            for detect in detections:
+                if detect.tag_id == 0:
+                    pt2d = detect.center
+                    camera_datapoints[f'cam{i}'].append((pt2d, pt3d))
+    
+    Ks = cameras.get_intrinsics()
+    camera_extrinsics = np.zeros((cameras.n_fixed_cameras, 4, 4))
+    for i in range(cameras.n_fixed_cameras):
+        pts2d = np.zeros((len(camera_datapoints[f'cam{i}']), 2))
+        pts3d = np.zeros((len(camera_datapoints[f'cam{i}']), 3))
+        for j in range(len(camera_datapoints[f'cam{i}'])):
+            pt2d, pt3d = camera_datapoints[f'cam{i}'][j]
+            pts2d[j, :] = pt2d
+            pts3d[j, :] = pt3d
+        K = Ks[i]
+        ret, rvec, tvec = cv2.solvePnP(pts3d, pts2d, K, distCoeffs=np.zeros(5))
+        
+        rotm = cv2.Rodrigues(rvec)[0]
+        H = np.eye(4)
+        H[:3, :3] = rotm
+        H[:3, 3] = tvec.flatten()
+        camera_extrinsics[i] = H
+        print(np.linalg.inv(H))
+        
+    import os
+    os.makedirs("../calib_data/extrinsics", exist_ok=True)
+    np.save("../calib_data/extrinsics/extrinsics.npy", camera_extrinsics)
+    print("Done")
+    
--- src/hardware/kuka.py
+++ src/hardware/kuka.py
@@ -19,6 +19,7 @@ from pydrake.all import (
     Diagram
 )
 from manipulation.station import MakeHardwareStation, MakeHardwareStationInterface, load_scenario
+import multiprocessing as mp
 
 def get_hardware_blocks(hardware_builder, scenario, meshcat = None, package_file='./package.xml'):
     real_station = hardware_builder.AddNamedSystem(
@@ -87,20 +88,77 @@ def create_hardware_diagram_plant(scenario_filepath, position_only=True, meshcat
     hardware_diagram = hardware_builder.Build()
     return hardware_diagram, hardware_plant, scene_graph_of_plant
 
-#NOTE: assume u are in graph_tracking/src when running
-class Kuka:
-    def __init__(self, scenario_file='../config/calib.yaml', package_file='../package.xml', position_only=True):
-        hardware_diagram, hardware_plant = create_hardware_diagram_plant(scenario_file, position_only=position_only, package_file=package_file)
-        self.hardware_diagram = hardware_diagram
-        self.hardware_plant = hardware_plant
-        self.plant_context = hardware_plant.CreateDefaultContext()
+def goto_joints(joint_thanos, endtime = 10.0, joint_speed = None):
+    root_builder = DiagramBuilder()
+    hardware_diagram, hardware_plant, _ = create_hardware_diagram_plant(scenario_filepath="../config/med.yaml",  position_only=True)
+    
+    hardware_block = root_builder.AddSystem(hardware_diagram)
+    
+    ## make a plan from current position to desired position
+    context = hardware_diagram.CreateDefaultContext()
+    hardware_diagram.ExecuteInitializationEvents(context)
+    curr_q = hardware_diagram.GetOutputPort("iiwa_thanos.position_measured").Eval(context)
+    
+    # speed is in rad/s
+    if joint_speed is not None:
+        max_dq = np.max(np.abs(joint_thanos - curr_q)) # get max a joint can move
+        endtime_from_speed = max_dq / joint_speed # get time to move at speed
+        endtime = max(endtime, endtime_from_speed)
+        endtime = endtime_from_speed
+        print(endtime_from_speed)
+    
+    ts = np.array([0.0, endtime])
+    qs = np.array([curr_q, joint_thanos])
+    traj = PiecewisePolynomial.FirstOrderHold(ts, qs.T)
         
-    def get_curr_joints(self):
-        context = self.hardware_diagram.CreateDefaultContext()
-        self.hardware_diagram.ExecuteInitializationEvents(context)
-        return self.hardware_diagram.GetOutputPort("iiwa_thanos.position_measured").Eval(context)
-        
-    def get_pose(self, frame_name="thanos_finger"):
-        curr_q = self.get_curr_joints()
-        self.hardware_plant.SetPositions(self.plant_context, curr_q)
-        return self.hardware_plant.GetFrameByName(frame_name).CalcPoseInWorld(self.plant_context)
+    traj_block = root_builder.AddSystem(TrajectorySource(traj))
+    root_builder.Connect(traj_block.get_output_port(), hardware_block.GetInputPort("iiwa_thanos.position"))
+
+    root_diagram = root_builder.Build()
+    
+    # run simulation
+    simulator = Simulator(root_diagram)
+    simulator.set_target_realtime_rate(1.0)
+    simulator.AdvanceTo(endtime + 2.0)
+    
+def goto_joints_mp(joint_thanos, endtime = 10.0, joint_speed = None):
+    fn = lambda joint_thanos, endtime, joint_speed: goto_joints(joint_thanos, endtime, joint_speed)
+    proc = mp.Process(target=fn, args=(joint_thanos, endtime, joint_speed))
+    proc.start()
+    proc.join()
+    
+def curr_joints_mp(scenario_file = "../config/med.yaml"):
+    q = mp.Queue()
+    def fn(scenario_file, q):
+        q.put(curr_joints(scenario_file))
+    proc = mp.Process(target=fn, args=(scenario_file, q))
+    proc.start()
+    proc.join()
+    return q.get()
+    
+def curr_joints(scenario_file = "../config/med.yaml"):
+    hardware_diagram, hardware_plant, _ = create_hardware_diagram_plant(scenario_filepath=scenario_file,  position_only=True)
+    context = hardware_diagram.CreateDefaultContext()
+    hardware_diagram.ExecuteInitializationEvents(context)
+    curr_q = hardware_diagram.GetOutputPort("iiwa_thanos.position_measured").Eval(context)
+    return curr_q
+
+def curr_pose(scenario_file = "../config/med.yaml", frame_name="iiwa_link_7"):
+    hardware_diagram, hardware_plant, _ = create_hardware_diagram_plant(scenario_filepath=scenario_file,  position_only=True)
+    context = hardware_diagram.CreateDefaultContext()
+    hardware_diagram.ExecuteInitializationEvents(context)
+    curr_q = hardware_diagram.GetOutputPort("iiwa_thanos.position_measured").Eval(context)
+    
+    plant_context = hardware_plant.CreateDefaultContext()
+    hardware_plant.SetPositions(plant_context, curr_q)
+    pose = hardware_plant.GetFrameByName(frame_name).CalcPoseInWorld(plant_context)
+    return pose
+
+def curr_pose_mp(scenario_file = "../config/med.yaml", frame_name="iiwa_link_7"):
+    q = mp.Queue()
+    def fn(scenario_file, frame_name, q):
+        q.put(curr_pose(scenario_file, frame_name))
+    proc = mp.Process(target=fn, args=(scenario_file, frame_name, q))
+    proc.start()
+    proc.join()
+    return q.get()
