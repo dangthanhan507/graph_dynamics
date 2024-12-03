3cb2f6cd46b8cb926ae64c7a844295c2fdcfbbdb
dangthanhan507 <dangthanhan507@gmail.com>
2024-12-03 02:59:58 -0500

    Added constant visual with zoom change. Visualization works perfectly now.

--- src/visualize.py
+++ src/visualize.py
@@ -32,6 +32,10 @@ if __name__ == '__main__':
     gnn.load_state_dict(torch.load(args.model_path, weights_only=True))
     gnn.eval()
     
+    vis = o3d.visualization.Visualizer()
+    vis.create_window(window_name='visualizer')
+    base_sphere_vertex = None
+    pcl = None
     with torch.no_grad():
         for i in range(len(dataset)):
             data,label = dataset[i]
@@ -43,14 +47,8 @@ if __name__ == '__main__':
             
             pcd, ptsrgb = visual_dataset[i+state_history]
             
-            # visualize spheres for out and label
-            origin = o3d.geometry.TriangleMesh.create_coordinate_frame(size=1.0, origin=[0, 0, 0])
-            vis = o3d.visualization.Visualizer()
-            vis.create_window(window_name='visualizer')
-            vis.add_geometry(origin)
-            
             workspace_bbox = np.array([[-0.3, 0.3],
-                                       [0.0, 1.0],
+                                       [0.3, 1.0],
                                        [-0.1, 0.5]])
             mask = (pcd[:,0] > workspace_bbox[0,0]) & (pcd[:,0] < workspace_bbox[0,1]) & \
                      (pcd[:,1] > workspace_bbox[1,0]) & (pcd[:,1] < workspace_bbox[1,1]) & \
@@ -58,35 +56,49 @@ if __name__ == '__main__':
             pcd = pcd[mask, :]
             ptsrgb = ptsrgb[mask, :]
             
-            
-            
+            # visualize spheres for out and label
+            # do initial visualization
+            if i == 0:
+                origin = o3d.geometry.TriangleMesh.create_coordinate_frame(size=0.3, origin=[0, 0, 0])
+                vis.add_geometry(origin)
+                out_spheres = []
+                for i in range(out.shape[0]):
+                    sphere = o3d.geometry.TriangleMesh.create_sphere(radius=0.005, resolution=10, create_uv_map=True)
+                    sphere.compute_vertex_normals()
+                    base_sphere_vertex = np.array(sphere.vertices)
+                    sphere.vertices = o3d.utility.Vector3dVector(base_sphere_vertex)
+                    out_spheres.append(sphere)
+                    vis.add_geometry(sphere)
+                # draw label spheres as yellow
+                label_spheres = []
+                for i in range(label.shape[0]):
+                    sphere = o3d.geometry.TriangleMesh.create_sphere(radius=0.005, resolution=10, create_uv_map=True)
+                    sphere.compute_vertex_normals()
+                    sphere.vertices = o3d.utility.Vector3dVector(base_sphere_vertex)
+                    sphere.paint_uniform_color([1, 1, 0])
+                    label_spheres.append(sphere)
+                    vis.add_geometry(sphere)
+            if pcl is not None:
+                # remove vis pcl and
+                vis.remove_geometry(pcl)
             
             # only consider point clouds inside the workspace
-            
             pcl = o3d.geometry.PointCloud()
             pcl.points = o3d.utility.Vector3dVector(pcd)
             pcl.colors = o3d.utility.Vector3dVector(ptsrgb/255)
             vis.add_geometry(pcl)
+            
             for i in range(out.shape[0]):
-                sphere = o3d.geometry.TriangleMesh.create_sphere(radius=0.005, resolution=10, create_uv_map=True)
-                sphere.compute_vertex_normals()
-                base_sphere_vertex = np.array(sphere.vertices)
-                sphere.vertices = o3d.utility.Vector3dVector(base_sphere_vertex + out[i,:])
-                vis.add_geometry(sphere)
-            # draw label spheres as yellow
-            for i in range(label.shape[0]):
-                sphere = o3d.geometry.TriangleMesh.create_sphere(radius=0.005, resolution=10, create_uv_map=True)
-                sphere.compute_vertex_normals()
-                base_sphere_vertex = np.array(sphere.vertices)
-                sphere.vertices = o3d.utility.Vector3dVector(base_sphere_vertex + label[i,:])
-                sphere.paint_uniform_color([1, 1, 0])
-                vis.add_geometry(sphere)
+                out_spheres[i].vertices = o3d.utility.Vector3dVector(base_sphere_vertex + out[i])
+                label_spheres[i].vertices = o3d.utility.Vector3dVector(base_sphere_vertex + label[i])
+                vis.update_geometry(out_spheres[i])
+                vis.update_geometry(label_spheres[i])
                 
-            # view_control = vis.get_view_control()
-            # view_control.set_front([1.0, 1.0, 1.0])
-            # view_control.set_lookat([0., 0.65, 0])
-            # view_control.set_up([0, 0, 1])
-            # view_control.set_zoom(1.6800000000000008) 
+            view_control = vis.get_view_control()
+            view_control.set_front([1.0, 1.0, 1.0])
+            view_control.set_lookat([0., 0.65, 0])
+            view_control.set_up([0, 0, 1])
+            view_control.set_zoom(0.400000000000008) 
             vis.poll_events()
             vis.update_renderer()
             vis.run()
